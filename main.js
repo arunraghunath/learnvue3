b8257e2e6028f8f264775dbcc53bfd0d226ce215
arunraghunath <arunattech@gmail.com>
2023-06-22 14:30:44 +0100

    Lesson - 2, Branch l2 - Attribute binding and conditional rendering

---
+++ main.js
@@ -0,0 +1,12 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            image: "./assets/images/socks_blue.jpg",
+            url: "https://www.vuemastery.com/",
+            inStock: false,
+            inventory: 0,
+            onSale: false
+        }
+    }
+})
